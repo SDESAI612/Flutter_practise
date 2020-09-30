50cfa13a76601afb049549a43a7e745c2847c486
SDESAI612 <shivdesai612@gmail.com>
2020-09-30 12:38:54 +0530

    Files Modified

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 
+
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'DrawerHome.dart';
 import 'UserModel.dart';
 import 'package:http/http.dart' as http;
@@ -78,12 +80,28 @@ class _MyHomePageState extends State<MyHomePage> {
   String username;
   String pass;
   UserModel _user;
+  bool check = false;
+  bool passCheck = true;
 
-  @override
+  void toggleVisibility() {
+   if(passCheck) {
+     setState(() {
+       passCheck = false;
+     });
+   }else {
+     setState(() {
+       passCheck=true;
+     });
+   }
+
+
+  }
+
+
+
+
+@override
   Widget build(BuildContext context) {
-
-
-
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
     //
@@ -98,77 +116,152 @@ class _MyHomePageState extends State<MyHomePage> {
           title: Text('Demo'),
         ),
         body: Center(
-          child: SingleChildScrollView (
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                  margin: const EdgeInsets.only(left: 20.0, right: 20.0),
-                  child: TextField(
-                    controller: emailController,
-                    cursorColor: Colors.amber,
-                    style: TextStyle(height: 1.0),
-                    cursorWidth: 3.0,
-                    decoration: new InputDecoration(
-                      focusedBorder: OutlineInputBorder(
-                          borderSide:
-                              BorderSide(color: Colors.greenAccent, width: 2.0),
-                          borderRadius:
-                              BorderRadius.all(new Radius.circular(20.0))),
-                      enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.red, width: 2.0),
-                          borderRadius:
-                              BorderRadius.all(new Radius.circular(20.0))),
-                      hintText: 'User Name',
-                    ),
-                  )),
-              Container(
-                  margin:
-                      const EdgeInsets.only(left: 20.0, right: 20.0, top: 20.0),
-                  child: TextField(
-                    controller: passController,
-                    cursorColor: Colors.amber,
-                    style: TextStyle(height: 1.0),
-                    cursorWidth: 3.0,
-                    decoration: new InputDecoration(
-                      focusedBorder: OutlineInputBorder(
-                          borderSide:
-                              BorderSide(color: Colors.greenAccent, width: 2.0),
-                          borderRadius:
-                              BorderRadius.all(new Radius.circular(20.0))),
-                      enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.red, width: 2.0),
-                          borderRadius:
-                              BorderRadius.all(new Radius.circular(20.0))),
-                      hintText: 'Password',
-                    ),
-                    obscureText: true,
-                  )),
-              Container(
-                margin: const EdgeInsets.only(top: 30.0),
-                color: Colors.red,
-                child: FlatButton(
-                  child: Text('Login'),
-                  onPressed: () async {
-                    username = emailController.text;
-                    pass = passController.text;
+            child: SingleChildScrollView(
+              child: Column(
+                // mainAxisAlignment: MainAxisAlignment.center,
+                // crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  // children: [Container(
+                  //   child: Icon(Icons.email,size: 16.00,),
+                  // ),
 
-                    // Route route = MaterialPageRoute(builder: (context) => Home(username,pass));
-                   final UserModel user = await createUser(username, pass);
-                    print(user.name);
-                   setState(() {
-                      _user = user;
-                   });
-                    Route route =
-                        MaterialPageRoute(builder: (context) => DrawerHome());
-                    Navigator.push(context, route);
-                  },
-                ),
+                  Container(
+                    margin: EdgeInsets.only(bottom: 24),
+                    child: ClipOval(
+                      child: Image.asset(
+                        'assets/loginIcon.png', height: 150, width: 150,
+
+                      ),
+                    ),
+                  ),
+
+                  Row(
+                    children: [
+                      Container(
+                        child: Icon(Icons.email, size: 40,),
+                        margin: EdgeInsets.only(left: 10),
+                      ),
+                      Expanded(
+                        flex: 1, // 20%
+                        child: Container(
+                            margin: const EdgeInsets.only(
+                                left: 20.0, right: 20.0),
+                            child: TextField(
+                              controller: emailController,
+                              cursorColor: Colors.amber,
+                              style: TextStyle(height: 1.0),
+                              cursorWidth: 3.0,
+                              decoration: new InputDecoration(
+                                focusedBorder: OutlineInputBorder(
+                                    borderSide:
+                                    BorderSide(
+                                        color: Colors.greenAccent, width: 2.0),
+                                    borderRadius:
+                                    BorderRadius.all(
+                                        new Radius.circular(10.0))),
+                                enabledBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                        color: Colors.red, width: 2.0),
+                                    borderRadius:
+                                    BorderRadius.all(
+                                        new Radius.circular(10.0))),
+                                hintText: 'User Name',
+                              ),
+                            )),
+                      ),
+                    ],
+
+                  ),
+
+                  Row(
+                    children: [
+                      Container(
+                        child: Icon(Icons.lock, size: 40,),
+                        margin: EdgeInsets.only(left: 10, top: 15),
+                      ),
+                      Expanded(
+                          flex: 1, // 20%
+                          child: Container(
+                              margin:
+                              const EdgeInsets.only(
+                                  left: 20.0, right: 20.0, top: 20.0),
+                              child: TextField(
+                                controller: passController,
+                                cursorColor: Colors.amber,
+                                style: TextStyle(height: 1.0),
+                                cursorWidth: 3.0,
+                                decoration: new InputDecoration(
+                                  suffixIcon: GestureDetector(
+                                    onTap: () {
+                                      toggleVisibility();
+                                    },
+                                    child: Icon(
+                                        passCheck ? Icons.visibility_off : Icons.visibility,
+                                      color: passCheck ? Colors.black : Colors.amber,
+                                    ),
+                                  ),
+                                  focusedBorder: OutlineInputBorder(
+                                      borderSide:
+                                      BorderSide(color: Colors.greenAccent,
+                                          width: 2.0),
+                                      borderRadius:
+                                      BorderRadius.all(
+                                          new Radius.circular(10.0))),
+                                  enabledBorder: OutlineInputBorder(
+                                      borderSide: BorderSide(
+                                          color: Colors.red, width: 2.0),
+                                      borderRadius:
+                                      BorderRadius.all(
+                                          new Radius.circular(10.0))),
+                                  hintText: 'Password',
+                                ),
+                                obscureText: passCheck,
+                              ))
+                      ),
+                    ],
+                  ),
+
+
+                  Container(
+                    margin: const EdgeInsets.only(top: 30.0),
+                    color: Colors.red,
+                    child: FlatButton(
+                      child: Text('Login'),
+                      onPressed: () async {
+                        username = emailController.text.trim();
+                        pass = passController.text.trim();
+
+                        // Route route = MaterialPageRoute(builder: (context) => Home(username,pass));
+                        // final UserModel user = await createUser(username, pass);
+                        //  print(user.name);
+                        // setState(() {
+                        //    _user = user;
+                        // });
+
+                        if (username == "Shiv" && pass == "Desai") {
+                          Route route =
+                          MaterialPageRoute(builder: (context) => DrawerHome());
+                          Navigator.push(context, route);
+                        } else {
+                          setState(() {
+                            check = true;
+                          });
+                        }
+                      },
+                    ),
+                  ),
+                  check ? Container(
+                    margin: EdgeInsets.only(top: 40.0),
+                    child:
+                    Text('Sorry username or Password is Incorrect',
+                      style: TextStyle(
+                          fontSize: 16.0,
+                          color: Colors.red
+                      ),),
+                  ) : Container()
+                ],
               ),
-               ],
-          ),
-        ))
+            ))
     );
   }
 }
